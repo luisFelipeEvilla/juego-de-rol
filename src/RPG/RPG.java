2586ad28b38c0de0aba007bec2fa627fde04e938
Luis Felipe Evilla <luisfelipe588@hotmail.com>
2020-03-18 19:55:25 -0500

    second thread created

--- src/RPG/RPG.java
+++ src/RPG/RPG.java
@@ -14,7 +14,7 @@ import javax.swing.JFrame;
  *
  * @author luisf
  */
-public class RPG extends Canvas {
+public class RPG extends Canvas implements Runnable {
         
     private static final int WIDE = 800;
     private static final int HIGHT = 600;
@@ -22,22 +22,38 @@ public class RPG extends Canvas {
     private static final String TITLE = "Juego de rol";
     
     private static JFrame window;
+    private static Thread thread;
     
     private RPG() {
         setPreferredSize(new Dimension(WIDE, HIGHT));
 
         window = new JFrame(TITLE);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE );
         window.setResizable(false);
         window.setLayout(new BorderLayout());
-        window.add(this, BorderLayout.CENTER);
-        window.pack();
+        window.add(this, BorderLayout.CENTER); // add canvas to the window and center it
+        window.pack(); //the items inside the window will fullfill it
         window.setLocationRelativeTo(null);
         window.setVisible(true);
     }
     
     public static void main(String[] args) {
         RPG app = new RPG();
+        app.start();
+    }
+
+    private void start() {
+        this.thread = new Thread(this, "Graphics");
+        thread.start();
+    }
+    
+    private void stop() {
+        
+    }
+    
+    @Override
+    public void run() {
+        // this will be called every time a new thread is started
     }
     
 }
